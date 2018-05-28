45abfe358e608c30cb2270f2c3c889487f98e77d
Yohannes Abera <yohannesns@gmail.com>
2018-05-28 00:14:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
-var playlist ={whitenyhousten: "I will always Love You"}
+var playlist = {whitenyhousten: "I will always Love You"}
+function updatePlaylist(playlist,'Teddy', 'Ethiopia'){
+  playlist[Teddy] = Ethiopia
+  return playlist
+}
